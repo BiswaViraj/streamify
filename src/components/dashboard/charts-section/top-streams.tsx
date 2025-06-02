3ba9f91651d5b06b7621f22067bf33f03c661cee
Biswajeet Das <biswajeetdas18@gmail.com>
2025-06-02 22:08:48 +0530

    feat: add TopStreams component for visualizing top streamed songs over the last 30 days

---
+++ src/components/dashboard/charts-section/top-streams.tsx
@@ -0,0 +1,71 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+import { useTopStreamedSongs } from "@/hooks/useTopStreamedSongs";
+import dayjs from "dayjs";
+import React from "react";
+import { Bar, BarChart, CartesianGrid, XAxis, YAxis } from "recharts";
+
+const startDate = dayjs().subtract(30, "day").toISOString();
+
+const CHART_CONFIG = {
+  streamCount: {
+    label: "Streams",
+  },
+};
+
+export default function TopStreams() {
+  const { topStreamedSongs } = useTopStreamedSongs({
+    startDate,
+    limit: 5,
+  });
+
+  const chartData = React.useMemo(() => {
+    return topStreamedSongs.map((song, index) => ({
+      title: song.title,
+      streamCount: song.streamCount,
+      fill: `var(--chart-${(index % 5) + 1})`,
+    }));
+  }, [topStreamedSongs]);
+
+  return (
+    <Card className="py-4 sm:py-0 col-span-2">
+      <CardHeader className="flex flex-col items-stretch border-b !p-0 sm:flex-row h-28">
+        <div className="flex flex-1 flex-col justify-center gap-1 px-6 pb-3 sm:pb-0">
+          <CardTitle>Top 5 songs</CardTitle>
+          <CardDescription>in the last 30days</CardDescription>
+        </div>
+      </CardHeader>
+      <CardContent className="px-2 sm:p-4">
+        <ChartContainer
+          config={CHART_CONFIG}
+          className="aspect-auto h-[250px] w-full"
+        >
+          <BarChart data={chartData}>
+            <ChartTooltip
+              content={<ChartTooltipContent nameKey="streamCount" />}
+            />
+            <CartesianGrid vertical={false} />
+            <XAxis dataKey="title" hide />
+            <YAxis
+              tickLine={false}
+              axisLine={false}
+              tickMargin={8}
+              dataKey="streamCount"
+            />
+            <Bar dataKey="streamCount" />
+          </BarChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  );
+}

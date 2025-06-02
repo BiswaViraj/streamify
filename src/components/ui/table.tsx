6d7529d53d1d318826371ae177b8dbf22f088f2a
Biswajeet Das <biswajeetdas18@gmail.com>
2025-06-02 22:10:55 +0530

    fix: reorder import statements for consistency

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -1,6 +1,5 @@
-import * as React from "react";
-
 import { cn } from "@/utils/style";
+import * as React from "react";
 
 function Table({ className, ...props }: React.ComponentProps<"table">) {
   return (
